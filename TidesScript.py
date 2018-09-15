7d91f63ef78f1f41758a97252af3d2d1baad9ca3
Geco <geco.hh@gmail.com>
2018-09-15 12:00:02 +0100

    smaller plot. It now actually sets as wallpaper

--- TidesScript.py
+++ TidesScript.py
@@ -1,15 +1,15 @@
 import urllib2
-import time
 from PIL import Image
 import os
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+import ctypes
+SPI_SETDESKWALLPAPER = 20 
 
 URL = "https://www.tide-forecast.com/tides/Puerto-de-la-Luz-Gran-Canaria-Canary-Islands.js"
-CURRENT_TIME = time.time()
 IMAGE_FOLDER = 'D:/Pictures/walls'
-IMAGE_OUTPUT_NAME = 'D:/Desktop/test_image.png'
+IMAGE_OUTPUT_NAME = 'D:/Pictures/walls/edited/edited.png'
 
 tides_as_list = urllib2.urlopen(URL).read().translate(None,'[] ;').split('\n')[:200]
 tide_by_time = map(lambda x: x.split(',')[2:4], tides_as_list)[1:-2]
@@ -30,12 +30,16 @@ plt.savefig(IMAGE_OUTPUT_NAME, bbox_inches='tight', transparent=True)
 
 #Resize to 1080
 image = Image.open(random_image)
-resized_wallpaper = image.resize((1920, 1080))
+wallpaper = image.resize((1920, 1080))
 
 #Attach plot to image
 plot_image = Image.open(IMAGE_OUTPUT_NAME)
 plot_image = plot_image.crop((20, 20, plot_image.width-20, plot_image.height-20))
+plot_image = plot_image.resize((int(plot_image.width*0.5), int(plot_image.height*0.5)))
 #Bottom right corner
-position = ((resized_wallpaper.width - plot_image.width), (resized_wallpaper.height - plot_image.height))
-resized_wallpaper.paste(plot_image, position, plot_image)
-resized_wallpaper.save(IMAGE_OUTPUT_NAME)
+position = ((wallpaper.width - plot_image.width), (wallpaper.height - plot_image.height)-50)
+wallpaper.paste(plot_image, position, plot_image)
+wallpaper.save(IMAGE_OUTPUT_NAME)
+
+#Set as wallpaper
+ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, IMAGE_OUTPUT_NAME , 0)
